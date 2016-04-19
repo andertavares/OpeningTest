aa1d0fad35a5fee6c9381e9842070e22571adf21
Anderson <andersonrochatavares@gmail.com>
2016-04-19 12:27:11 -0300

    getting ready to parse a xml configuration file

--- SCProjects/OpprimoBot/Source/Data/Configuration.cpp
+++ SCProjects/OpprimoBot/Source/Data/Configuration.cpp
@@ -1,9 +1,46 @@
 #include "Configuration.h"
+#include "../Utils/tinyxml2.h"
+#include <BWAPI.h>
 
+//change these if you need to read/write in different folders
+const string Configuration::INPUT_DIR = "bwapi-data/AI/";
+const string Configuration::OUTPUT_DIR = "bwapi-data/AI/";
+const string Configuration::CONFIG_FILE = Configuration::INPUT_DIR + "config.xml";
+
+Configuration* Configuration::instance = NULL;
 
 Configuration::Configuration() {
+	//sets up default values
+	matchDataFile = OUTPUT_DIR + "output.xml";
+	buildOrderID = "Quick Factory Mines";
+}
+
+Configuration* Configuration::getInstance() {
+	if (instance == NULL) {
+		instance = new Configuration();
+	}
+	return instance;
 }
 
 
 Configuration::~Configuration() {
+}
+
+
+void Configuration::parseConfig() {
+	using namespace tinyxml2;
+	using namespace BWAPI;
+
+	XMLDocument doc;
+	int result = doc.LoadFile(CONFIG_FILE.c_str());
+
+	if (result != XML_NO_ERROR) {
+		Broodwar->printf("An error has occurred while parsing the configuration file '%s'. Error: '%s'", CONFIG_FILE.c_str(), doc.ErrorName());
+		return;
+	}
+
+	//if matchDataFile <<
+	//if buildOrderID <<
+
+
 }
--- SCProjects/OpprimoBot/Source/Data/Configuration.h
+++ SCProjects/OpprimoBot/Source/Data/Configuration.h
@@ -12,9 +12,27 @@ class Configuration {
 	//path to write the file with match details
 	string matchDataFile;
 
-public:
+	//the singleton instance of this class
+	static Configuration* instance;
+
 	Configuration();
+
+public:
+	//Returns the singleton instance of this class
+	Configuration* getInstance();
+
 	~Configuration();
+
+	void parseConfig();
+
+	//Directory to read input from
+	static const string INPUT_DIR;
+
+	//Directory to write output to
+	static const string OUTPUT_DIR;
+
+	//File to read configs from
+	static const string CONFIG_FILE;
 };
 
 #endif
