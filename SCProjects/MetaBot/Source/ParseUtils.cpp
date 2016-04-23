9b768e4bbf7bfde6c9ee8b393699eb312a3577af
Amanda Santos <amanda.santos20@hotmail.com>
2016-04-23 12:18:57 -0300

    TextParserStrategy ready.

--- SCProjects/MetaBot/Source/ParseUtils.cpp
+++ SCProjects/MetaBot/Source/ParseUtils.cpp
@@ -21,6 +21,12 @@ void ParseUtils::ParseConfigFile(const std::string & filename, std::vector<Build
         BWAPI::Broodwar->printf("Could not open file: %s", filename.c_str());
     }
 
+    bool parsingFailed = doc.Parse(text.c_str()).HasParseError();
+    if (parsingFailed) {
+        Broodwar->printf("Parsing Failed");
+        return;
+    }
+
     if (text.length() == 0) {
         return;
     }
@@ -28,24 +34,39 @@ void ParseUtils::ParseConfigFile(const std::string & filename, std::vector<Build
     if (doc.HasMember("Build name") && doc["Build name"].IsObject()) {
         const rapidjson::Value &info = doc["Build name"];
         std::vector<char*> commandTypes{
-            "CommandCenter",
-            "ComsatStation",
-            "NuclearSilo",
-            "SupplyDepot",
+            "Command Center",
+            "Comsat Station",
+            "Nuclear Silo",
+            "Supply Depot",
             "Barracks",
             "Refinery",
-            "EngineeringBay",
+            "Engineering Bay",
             "Bunker",
             "Academy",
-            "MissileTurret",
+            "Missile Turret",
             "Factory",
-            "MachineShop",
+            "Machine Shop",
             "Starport",
-            "ControlTower",
+            "Control Tower",
             "Armory",
-            "ScienceFacility",
-            "PhysicsLab",
-            "CovertOps"
+            "Science Facility",
+            "Physics Lab",
+            "Covert Ops",
+            "StimPacks",
+            "Personnel Cloaking",
+            "Lockdown",
+            "Tank Siege Mode",
+            "EMP Shockwave",
+            "Yamato Gun",
+            "Terran Infantry Armor",
+            "Terran Infantry Weapons",
+            "Terran Ship Plating",
+            "Terran Ship Weapons",
+            "Terran Vehicle Plating",
+            "Terran Vehicle Weapons",
+            "Charon Boosters",
+            "Caduceus Reactor",
+            "U238 Shells"
         };
 
         for (const auto& commandType : commandTypes) {
@@ -60,16 +81,13 @@ void ParseUtils::ParseConfigFile(const std::string & filename, std::vector<Build
                 int type = ParseType(commandType, unittype, techtype, upgradetype);
 
                 if (type == UnitCode) {
-					Broodwar->printf("Pushing %s@%d to build plan", unittype.c_str(), atSupply);
-                    buildplan.push_back(BuildplanEntry(unittype, atSupply));
+					buildplan.push_back(BuildplanEntry(unittype, atSupply));
                 }
                 else if (type == UpgradeCode) {
-					Broodwar->printf("Pushing %s@%d to build plan", upgradetype.c_str(), atSupply);
-                    buildplan.push_back(BuildplanEntry(upgradetype, atSupply));
+					buildplan.push_back(BuildplanEntry(upgradetype, atSupply));
                 }
                 else if (type == TechCode) {
-					Broodwar->printf("Pushing %s@%d to build plan", techtype.c_str(), atSupply);
-                    buildplan.push_back(BuildplanEntry(techtype, atSupply));
+					buildplan.push_back(BuildplanEntry(techtype, atSupply));
                 }
             }
         }
@@ -78,19 +96,19 @@ void ParseUtils::ParseConfigFile(const std::string & filename, std::vector<Build
 
 
 int ParseUtils::ParseType(const std::string& commandType, UnitType& unitype, TechType& techtype, UpgradeType& upgradetype) {
-    if (commandType == "CommandCenter") {
+    if (commandType == "Command Center") {
         unitype = BWAPI::UnitTypes::Terran_Command_Center;
         return ParseUtils::UnitCode;
     }
-    else if (commandType == "ComsatStation") {
+    else if (commandType == "Comsat Station") {
         unitype = BWAPI::UnitTypes::Terran_Comsat_Station;
         return ParseUtils::UnitCode;
     }
-    else if (commandType == "NuclearSilo") {
+    else if (commandType == "Nuclear Silo") {
         unitype = BWAPI::UnitTypes::Terran_Nuclear_Silo;
         return ParseUtils::UnitCode;
     }
-    else if (commandType == "SupplyDepot") {
+    else if (commandType == "Supply Depot") {
         unitype = BWAPI::UnitTypes::Terran_Supply_Depot;
         return ParseUtils::UnitCode;
     }
@@ -102,7 +120,7 @@ int ParseUtils::ParseType(const std::string& commandType, UnitType& unitype, Tec
         unitype = BWAPI::UnitTypes::Terran_Refinery;
         return ParseUtils::UnitCode;
     }
-    else if (commandType == "EngineeringBay") {
+    else if (commandType == "Engineering Bay") {
         unitype = BWAPI::UnitTypes::Terran_Engineering_Bay;
         return ParseUtils::UnitCode;
     }
@@ -114,7 +132,7 @@ int ParseUtils::ParseType(const std::string& commandType, UnitType& unitype, Tec
         unitype = BWAPI::UnitTypes::Terran_Academy;
         return ParseUtils::UnitCode;
     }
-    else if (commandType == "MissileTurret") {
+    else if (commandType == "Missile Turret") {
         unitype = BWAPI::UnitTypes::Terran_Missile_Turret;
         return ParseUtils::UnitCode;
     }
@@ -122,7 +140,7 @@ int ParseUtils::ParseType(const std::string& commandType, UnitType& unitype, Tec
         unitype = BWAPI::UnitTypes::Terran_Factory;
         return ParseUtils::UnitCode;
     }
-    else if (commandType == "MachineShop") {
+    else if (commandType == "Machine Shop") {
         unitype = BWAPI::UnitTypes::Terran_Machine_Shop;
         return ParseUtils::UnitCode;
     }
@@ -130,7 +148,7 @@ int ParseUtils::ParseType(const std::string& commandType, UnitType& unitype, Tec
         unitype = BWAPI::UnitTypes::Terran_Starport;
         return ParseUtils::UnitCode;
     }
-    else if (commandType == "ControlTower") {
+    else if (commandType == "Control Tower") {
         unitype = BWAPI::UnitTypes::Terran_Control_Tower;
         return ParseUtils::UnitCode;
     }
@@ -138,23 +156,23 @@ int ParseUtils::ParseType(const std::string& commandType, UnitType& unitype, Tec
         unitype = BWAPI::UnitTypes::Terran_Armory;
         return ParseUtils::UnitCode;
     }
-    else if (commandType == "ScienceFacility") {
+    else if (commandType == "Science Facility") {
         unitype = BWAPI::UnitTypes::Terran_Science_Facility;
         return ParseUtils::UnitCode;
     }
-    else if (commandType == "PhysicsLab") {
+    else if (commandType == "Physics Lab") {
         unitype = BWAPI::UnitTypes::Terran_Physics_Lab;
         return ParseUtils::UnitCode;
     }
-    else if (commandType == "CovertOps") {
+    else if (commandType == "Covert Ops") {
         unitype = BWAPI::UnitTypes::Terran_Covert_Ops;
         return ParseUtils::UnitCode;
     }
-    else if (commandType == "StimPacks") {
+    else if (commandType == "Stim Packs") {
         techtype = BWAPI::TechTypes::Stim_Packs;
         return ParseUtils::TechCode;
     }
-    else if (commandType == "PersonnelCloaking") {
+    else if (commandType == "Personnel Cloaking") {
         techtype = BWAPI::TechTypes::Personnel_Cloaking;
         return ParseUtils::TechCode;
     }
@@ -162,51 +180,51 @@ int ParseUtils::ParseType(const std::string& commandType, UnitType& unitype, Tec
         techtype = BWAPI::TechTypes::Lockdown;
         return ParseUtils::TechCode;
     }
-    else if (commandType == "TankSiegeMode") {
+    else if (commandType == "Tank Siege Mode") {
         techtype = BWAPI::TechTypes::Tank_Siege_Mode;
         return ParseUtils::TechCode;
     }
-    else if (commandType == "EMPShockwave") {
+    else if (commandType == "EMP Shockwave") {
         techtype = BWAPI::TechTypes::EMP_Shockwave;
         return ParseUtils::TechCode;
     }
-    else if (commandType == "YamatoGun") {
+    else if (commandType == "Yamato Gun") {
         techtype = BWAPI::TechTypes::Yamato_Gun;
         return ParseUtils::TechCode;
     }
-    else if (commandType == "TerranInfantryArmor") {
+    else if (commandType == "Terran Infantry Armor") {
         upgradetype = BWAPI::UpgradeTypes::Terran_Infantry_Armor;
         return ParseUtils::UpgradeCode;
     }
-    else if (commandType == "TerranInfantryWeapons") {
+    else if (commandType == "Terran Infantry Weapons") {
         upgradetype = BWAPI::UpgradeTypes::Terran_Infantry_Weapons;
         return ParseUtils::UpgradeCode;
     }
-    else if (commandType == "TerranShipPlating") {
+    else if (commandType == "Terran Ship Plating") {
         upgradetype = BWAPI::UpgradeTypes::Terran_Ship_Plating;
         return ParseUtils::UpgradeCode;
     }
-    else if (commandType == "TerranShipWeapons") {
+    else if (commandType == "Terran Ship Weapons") {
         upgradetype = BWAPI::UpgradeTypes::Terran_Ship_Weapons;
         return ParseUtils::UpgradeCode;
     }
-    else if (commandType == "TerranVehiclePlating") {
+    else if (commandType == "Terran Vehicle Plating") {
         upgradetype = BWAPI::UpgradeTypes::Terran_Vehicle_Plating;
         return ParseUtils::UpgradeCode;
     }
-    else if (commandType == "TerranVehicleWeapons") {
+    else if (commandType == "Terran Vehicle Weapons") {
         upgradetype = BWAPI::UpgradeTypes::Terran_Vehicle_Weapons;
         return ParseUtils::UpgradeCode;
     }
-    else if (commandType == "CharonBoosters") {
+    else if (commandType == "Charon Boosters") {
         upgradetype = BWAPI::UpgradeTypes::Charon_Boosters;
         return ParseUtils::UpgradeCode;
     }
-    else if (commandType == "CaduceusReactor") {
+    else if (commandType == "Caduceus Reactor") {
         upgradetype = BWAPI::UpgradeTypes::Caduceus_Reactor;
         return ParseUtils::UpgradeCode;
     }
-    else if (commandType == "U238Shells") {
+    else if (commandType == "U238 Shells") {
         upgradetype = BWAPI::UpgradeTypes::U_238_Shells;
         return ParseUtils::UpgradeCode;
     }
