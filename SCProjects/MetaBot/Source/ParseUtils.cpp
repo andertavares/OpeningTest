7200bff6f4cd23215e9fc0f0f0d9ce6a174bc928
Anderson <andersonrochatavares@gmail.com>
2016-04-23 14:39:12 -0300

    using namespaces to improve readability

--- SCProjects/MetaBot/Source/ParseUtils.cpp
+++ SCProjects/MetaBot/Source/ParseUtils.cpp
@@ -2,12 +2,15 @@
 
 #include <vector>
 
-void ParseUtils::ParseConfigFile(const std::string & filename, std::vector<BuildplanEntry>& buildplan) {
-    rapidjson::Document doc;
-    BWAPI::Race race = BWAPI::Races::Terran;
+using namespace std;
+using namespace BWAPI;
 
-    std::string text;
-    std::ifstream inputFile(filename);
+void ParseUtils::ParseConfigFile(const string & filename, vector<BuildplanEntry>& buildplan) {
+    rapidjson::Document doc;
+    Race race = Races::Terran;
+
+    string text;
+    ifstream inputFile(filename);
     if (inputFile.is_open()) {
         std::string line; /* or other suitable maximum line size */
         do {
@@ -18,7 +21,7 @@ void ParseUtils::ParseConfigFile(const std::string & filename, std::vector<Build
         inputFile.close();
     }
     else {
-        BWAPI::Broodwar->printf("Could not open file: %s", filename.c_str());
+        Broodwar->printf("Could not open file: %s", filename.c_str());
     }
 
     bool parsingFailed = doc.Parse(text.c_str()).HasParseError();
@@ -73,9 +76,9 @@ void ParseUtils::ParseConfigFile(const std::string & filename, std::vector<Build
             std::vector<int> atSupplies;
             JSONTools::ReadArray(commandType, info, atSupplies);
 
-            BWAPI::UnitType unittype;
-            BWAPI::TechType techtype;
-            BWAPI::UpgradeType upgradetype;
+            UnitType unittype;
+            TechType techtype;
+            UpgradeType upgradetype;
 
             for (const auto& atSupply : atSupplies) {
                 int type = ParseType(commandType, unittype, techtype, upgradetype);
@@ -97,135 +100,135 @@ void ParseUtils::ParseConfigFile(const std::string & filename, std::vector<Build
 
 int ParseUtils::ParseType(const std::string& commandType, UnitType& unitype, TechType& techtype, UpgradeType& upgradetype) {
     if (commandType == "Command Center") {
-        unitype = BWAPI::UnitTypes::Terran_Command_Center;
+        unitype = UnitTypes::Terran_Command_Center;
         return ParseUtils::UnitCode;
     }
     else if (commandType == "Comsat Station") {
-        unitype = BWAPI::UnitTypes::Terran_Comsat_Station;
+        unitype = UnitTypes::Terran_Comsat_Station;
         return ParseUtils::UnitCode;
     }
     else if (commandType == "Nuclear Silo") {
-        unitype = BWAPI::UnitTypes::Terran_Nuclear_Silo;
+        unitype = UnitTypes::Terran_Nuclear_Silo;
         return ParseUtils::UnitCode;
     }
     else if (commandType == "Supply Depot") {
-        unitype = BWAPI::UnitTypes::Terran_Supply_Depot;
+        unitype = UnitTypes::Terran_Supply_Depot;
         return ParseUtils::UnitCode;
     }
     else if (commandType == "Barracks") {
-        unitype = BWAPI::UnitTypes::Terran_Barracks;
+        unitype = UnitTypes::Terran_Barracks;
         return ParseUtils::UnitCode;
     }
     else if (commandType == "Refinery") {
-        unitype = BWAPI::UnitTypes::Terran_Refinery;
+        unitype = UnitTypes::Terran_Refinery;
         return ParseUtils::UnitCode;
     }
     else if (commandType == "Engineering Bay") {
-        unitype = BWAPI::UnitTypes::Terran_Engineering_Bay;
+        unitype = UnitTypes::Terran_Engineering_Bay;
         return ParseUtils::UnitCode;
     }
     else if (commandType == "Bunker") {
-        unitype = BWAPI::UnitTypes::Terran_Bunker;
+        unitype = UnitTypes::Terran_Bunker;
         return ParseUtils::UnitCode;
     }
     else if (commandType == "Academy") {
-        unitype = BWAPI::UnitTypes::Terran_Academy;
+        unitype = UnitTypes::Terran_Academy;
         return ParseUtils::UnitCode;
     }
     else if (commandType == "Missile Turret") {
-        unitype = BWAPI::UnitTypes::Terran_Missile_Turret;
+        unitype = UnitTypes::Terran_Missile_Turret;
         return ParseUtils::UnitCode;
     }
     else if (commandType == "Factory") {
-        unitype = BWAPI::UnitTypes::Terran_Factory;
+        unitype = UnitTypes::Terran_Factory;
         return ParseUtils::UnitCode;
     }
     else if (commandType == "Machine Shop") {
-        unitype = BWAPI::UnitTypes::Terran_Machine_Shop;
+        unitype = UnitTypes::Terran_Machine_Shop;
         return ParseUtils::UnitCode;
     }
     else if (commandType == "Starport") {
-        unitype = BWAPI::UnitTypes::Terran_Starport;
+        unitype = UnitTypes::Terran_Starport;
         return ParseUtils::UnitCode;
     }
     else if (commandType == "Control Tower") {
-        unitype = BWAPI::UnitTypes::Terran_Control_Tower;
+        unitype = UnitTypes::Terran_Control_Tower;
         return ParseUtils::UnitCode;
     }
     else if (commandType == "Armory") {
-        unitype = BWAPI::UnitTypes::Terran_Armory;
+        unitype = UnitTypes::Terran_Armory;
         return ParseUtils::UnitCode;
     }
     else if (commandType == "Science Facility") {
-        unitype = BWAPI::UnitTypes::Terran_Science_Facility;
+        unitype = UnitTypes::Terran_Science_Facility;
         return ParseUtils::UnitCode;
     }
     else if (commandType == "Physics Lab") {
-        unitype = BWAPI::UnitTypes::Terran_Physics_Lab;
+        unitype = UnitTypes::Terran_Physics_Lab;
         return ParseUtils::UnitCode;
     }
     else if (commandType == "Covert Ops") {
-        unitype = BWAPI::UnitTypes::Terran_Covert_Ops;
+        unitype = UnitTypes::Terran_Covert_Ops;
         return ParseUtils::UnitCode;
     }
     else if (commandType == "Stim Packs") {
-        techtype = BWAPI::TechTypes::Stim_Packs;
+        techtype = TechTypes::Stim_Packs;
         return ParseUtils::TechCode;
     }
     else if (commandType == "Personnel Cloaking") {
-        techtype = BWAPI::TechTypes::Personnel_Cloaking;
+        techtype = TechTypes::Personnel_Cloaking;
         return ParseUtils::TechCode;
     }
     else if (commandType == "Lockdown") {
-        techtype = BWAPI::TechTypes::Lockdown;
+        techtype = TechTypes::Lockdown;
         return ParseUtils::TechCode;
     }
     else if (commandType == "Tank Siege Mode") {
-        techtype = BWAPI::TechTypes::Tank_Siege_Mode;
+        techtype = TechTypes::Tank_Siege_Mode;
         return ParseUtils::TechCode;
     }
     else if (commandType == "EMP Shockwave") {
-        techtype = BWAPI::TechTypes::EMP_Shockwave;
+        techtype = TechTypes::EMP_Shockwave;
         return ParseUtils::TechCode;
     }
     else if (commandType == "Yamato Gun") {
-        techtype = BWAPI::TechTypes::Yamato_Gun;
+        techtype = TechTypes::Yamato_Gun;
         return ParseUtils::TechCode;
     }
     else if (commandType == "Terran Infantry Armor") {
-        upgradetype = BWAPI::UpgradeTypes::Terran_Infantry_Armor;
+        upgradetype = UpgradeTypes::Terran_Infantry_Armor;
         return ParseUtils::UpgradeCode;
     }
     else if (commandType == "Terran Infantry Weapons") {
-        upgradetype = BWAPI::UpgradeTypes::Terran_Infantry_Weapons;
+        upgradetype = UpgradeTypes::Terran_Infantry_Weapons;
         return ParseUtils::UpgradeCode;
     }
     else if (commandType == "Terran Ship Plating") {
-        upgradetype = BWAPI::UpgradeTypes::Terran_Ship_Plating;
+        upgradetype = UpgradeTypes::Terran_Ship_Plating;
         return ParseUtils::UpgradeCode;
     }
     else if (commandType == "Terran Ship Weapons") {
-        upgradetype = BWAPI::UpgradeTypes::Terran_Ship_Weapons;
+        upgradetype = UpgradeTypes::Terran_Ship_Weapons;
         return ParseUtils::UpgradeCode;
     }
     else if (commandType == "Terran Vehicle Plating") {
-        upgradetype = BWAPI::UpgradeTypes::Terran_Vehicle_Plating;
+        upgradetype = UpgradeTypes::Terran_Vehicle_Plating;
         return ParseUtils::UpgradeCode;
     }
     else if (commandType == "Terran Vehicle Weapons") {
-        upgradetype = BWAPI::UpgradeTypes::Terran_Vehicle_Weapons;
+        upgradetype = UpgradeTypes::Terran_Vehicle_Weapons;
         return ParseUtils::UpgradeCode;
     }
     else if (commandType == "Charon Boosters") {
-        upgradetype = BWAPI::UpgradeTypes::Charon_Boosters;
+        upgradetype = UpgradeTypes::Charon_Boosters;
         return ParseUtils::UpgradeCode;
     }
     else if (commandType == "Caduceus Reactor") {
-        upgradetype = BWAPI::UpgradeTypes::Caduceus_Reactor;
+        upgradetype = UpgradeTypes::Caduceus_Reactor;
         return ParseUtils::UpgradeCode;
     }
     else if (commandType == "U238 Shells") {
-        upgradetype = BWAPI::UpgradeTypes::U_238_Shells;
+        upgradetype = UpgradeTypes::U_238_Shells;
         return ParseUtils::UpgradeCode;
     }
 
